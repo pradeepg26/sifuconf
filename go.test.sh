c705bab4c76aa49d7e4f7e4d2f1df7f37776bbd6
Pradeep Gollakota <pgollakota@fitbit.com>
2018-01-11 09:32:29 -0800

    Fix junit report generation

--- go.test.sh
+++ go.test.sh
@@ -4,9 +4,10 @@ set -e
 echo "" > coverage.txt
 
 for d in $(go list ./... | grep -v vendor); do
-    go test -race -coverprofile=profile.out -covermode=atomic $d | go-junit-report > $CIRCLE_TEST_REPORTS/${d}_report.xml
-    if [ -f profile.out ]; then
-        cat profile.out >> coverage.txt
-        rm profile.out
-    fi
+  mkdir -p $CIRCLE_TEST_REPORTS/$d
+  go test -race -coverprofile=profile.out -covermode=atomic $d | go-junit-report > $CIRCLE_TEST_REPORTS/${d}_report.xml
+  if [ -f profile.out ]; then
+    cat profile.out >> coverage.txt
+    rm profile.out
+  fi
 done
