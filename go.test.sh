99eabdb30cc0fac360641762b614961698ce01e9
Pradeep Gollakota <pgollakota@fitbit.com>
2018-01-11 13:14:38 -0800

    Fix test reporting

--- go.test.sh
+++ go.test.sh
@@ -2,12 +2,15 @@
 
 set -e
 echo "" > coverage.txt
+rm -f $CIRCLE_TEST_REPORTS/report.xml
 
 for d in $(go list ./... | grep -v vendor); do
-  mkdir -p ./reports/$d
-  go test -race -coverprofile=profile.out -covermode=atomic $d | go-junit-report > ./reports/${d}_report.xml
+  mkdir -p $CIRCLE_TEST_REPORTS/$d
+  go test -v -race -coverprofile=profile.out -covermode=atomic $d | go-junit-report > ./$CIRCLE_TEST_REPORTS/${d}_report.xml
   if [ -f profile.out ]; then
     cat profile.out >> coverage.txt
     rm profile.out
   fi
 done
+
+junit-merge -r -o $CIRCLE_TEST_REPORTS/report.xml $CIRCLE_TEST_REPORTS
