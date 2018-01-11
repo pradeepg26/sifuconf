c2f921255d2adab48a2b6a383a44e6f31863afa7
Pradeep Gollakota <pgollakota@fitbit.com>
2018-01-11 13:17:06 -0800

    Fix test report path in go.test.sh

--- go.test.sh
+++ go.test.sh
@@ -6,7 +6,7 @@ rm -f $CIRCLE_TEST_REPORTS/report.xml
 
 for d in $(go list ./... | grep -v vendor); do
   mkdir -p $CIRCLE_TEST_REPORTS/$d
-  go test -v -race -coverprofile=profile.out -covermode=atomic $d | go-junit-report > ./$CIRCLE_TEST_REPORTS/${d}_report.xml
+  go test -v -race -coverprofile=profile.out -covermode=atomic $d | go-junit-report > $CIRCLE_TEST_REPORTS/${d}_report.xml
   if [ -f profile.out ]; then
     cat profile.out >> coverage.txt
     rm profile.out
