317710de5890f89824339d30dc1fa677dbbbe527
Pradeep Gollakota <pgollakota@fitbit.com>
2018-01-11 09:26:57 -0800

    Generate junit test reports in addition to codecov

--- go.test.sh
+++ go.test.sh
@@ -4,7 +4,7 @@ set -e
 echo "" > coverage.txt
 
 for d in $(go list ./... | grep -v vendor); do
-    go test -race -coverprofile=profile.out -covermode=atomic $d
+    go test -race -coverprofile=profile.out -covermode=atomic $d | go-junit-report > $CIRCLE_TEST_REPORTS/${d}_report.xml
     if [ -f profile.out ]; then
         cat profile.out >> coverage.txt
         rm profile.out
