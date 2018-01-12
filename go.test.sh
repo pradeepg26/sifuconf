c98a6898c3960697608c0918790badd63d87b1ba
Pradeep Gollakota <pgollakota@fitbit.com>
2018-01-11 21:16:22 -0800

    Fixed merging of junit results

--- go.test.sh
+++ go.test.sh
@@ -5,12 +5,14 @@ echo "" > coverage.txt
 rm -f $CIRCLE_TEST_REPORTS/report.xml
 
 for d in $(go list ./... | grep -v vendor); do
-  mkdir -p $CIRCLE_TEST_REPORTS/$d
-  go test -v -race -coverprofile=profile.out -covermode=atomic $d | go-junit-report > $CIRCLE_TEST_REPORTS/${d}_report.xml
+  mkdir -p $CIRCLE_TEST_REPORTS/tmp/$d
+  go test -v -race -coverprofile=profile.out -covermode=atomic $d | go-junit-report > $CIRCLE_TEST_REPORTS/tmp/${d}_report.xml
   if [ -f profile.out ]; then
     cat profile.out >> coverage.txt
     rm profile.out
   fi
 done
 
-junit-merge -r -o $CIRCLE_TEST_REPORTS/report.xml $CIRCLE_TEST_REPORTS
+FILES=$(find $CIRCLE_TEST_REPORTS/tmp/ -name '*.xml')
+junit-merge -o $CIRCLE_TEST_REPORTS/report.xml $FILES
+rm -rf $CIRCLE_TEST_REPORTS/tmp
